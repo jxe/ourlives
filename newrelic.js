53a0e2a8ec0b21f402b4cddbcca57027b709cd91
Joe Edelman <joe@citizenlogistics.com>
2014-02-02 17:22:19 -0800

    the basics

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['Sandbar'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '7d6b1fc7ab457408d22ff97811f270ca5eb3b076',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'trace'
+  }
+};
